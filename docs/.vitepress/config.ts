c8dbc5974d285e78729efd7be8d906a8157015ed
curder <q.curder@gmail.com>
2025-08-05 13:37:53 +0800

    feat: 初始化微信小程序学习项目
    添加基础项目配置文件和文档结构，包括.nvmrc、.gitignore、package.json和vitepress文档配置

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  lang: "zh-CN",
+  base: "/wechat-miniprogram-study/",
+  title: "微信小程序学习",
+  description: "微信小程序学习，记录一些工作中常用的代码片段。",
+  lastUpdated: true,
+  themeConfig: {
+    logo: "",
+    siteTitle: "微信小程序学习",
+    outline: {
+      label: "章节导航",
+      level: "deep",
+    },
+    lastUpdated: {
+      text: "最后更新时间",
+    },
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    editLink: {
+      pattern:
+        "https://github.com/curder/wechat-miniprogram-study/edit/master/docs/:path",
+      text: "编辑它",
+    },
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/curder/wechat-miniprogram-study",
+      },
+    ],
+    nav: nav(),
+    sidebar: {
+      "/basic": sidebarBasic(),
+    },
+  },
+});
+
+function nav() {
+  return [
+    //
+  ];
+}
+
+function sidebarBasic() {
+  return [
+    //
+  ];
+}
